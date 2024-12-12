16f5b34a74540d27cf280a20f2ce26f81c409e1b
Julian Currie <julian.currie@gmail.com>
2024-12-12 15:02:56 +0700

    added chakra button component, dashboard page, and Jotai session atom

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,15 @@
+'use client';
+import { Button, Center, Separator, Text } from "@chakra-ui/react";
+
+function Dashboard() {
+  return (
+    <>
+      <Center>
+        <Text fontSize='6xl'>Your Decks</Text>
+      </Center>
+      <div className="border-t border-gray-100 my-8 mx-8" />
+    </>
+  );
+}
+
+export default Dashboard;;
---
+++ app/store/auth.ts
@@ -0,0 +1,5 @@
+import { atom } from "jotai";
+import { Session } from "@supabase/supabase-js";
+
+export const sessionAtom = atom<Session | null>(null);
+
